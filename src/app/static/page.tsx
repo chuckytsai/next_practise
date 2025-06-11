6809e1103cbee5e24960e8db2a2babc4352766fb
Chucky.Tsai <64692566+chuckytsai@users.noreply.github.com>
2025-06-11 09:57:06 +0800

    [ feature ] 不建議SSG去讀取自己專案內的API

--- src/app/static/page.tsx
+++ src/app/static/page.tsx
@@ -1,13 +1,13 @@
-export default async function StaticExample() {
-  const res = await fetch('http://localhost:3000/api/hello', {
-    cache: 'force-cache', // ✅ 等同於 getStaticProps 的效果
+export default async function StaticPage() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts/1', {
+    cache: 'force-cache',
   })
   const data = await res.json()
 
   return (
     <div>
       <h1>App Router - Static Page</h1>
-      <p>從 API 拿到的資料：{data.message}</p>
+      <p>{data.title}</p>
     </div>
   )
-};
+}
